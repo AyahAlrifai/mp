5a5a1ceb3a1ce22ef6f506a64412fa494d768c25
AyahRefai <arefai@blulogix.com>
2023-03-09 14:16:06 +0300

    v10

--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -5,37 +5,37 @@ import { Observable } from "rxjs";
 @Injectable()
 export class CustomerService {
     private host: string = "http://localhost:8080/CIS-SERVER/";
-    public  headers:any = {
+    public headers: any = {
         "Accept": "application/json, text/plain, */*",
         "Access-Control-Allow-Origin": "http://localhost:8080",
-        "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJaOUF1bG9iSlQ2Mmd2al9mX1VwOGp0elcwVWM4dGRiVF8zYkZab3NSbWJvIn0.eyJleHAiOjE2NzgzNDgwODcsImlhdCI6MTY3ODE3NTI4NywianRpIjoiZWRhZDE0N2QtMTAwMC00MTdmLTk1NmQtMDMzYmY0ODk5MTRlIiwiaXNzIjoiaHR0cDovLzE5Mi4xNjguMS44NDo4MDgwL2F1dGgvcmVhbG1zL2Npcy1wLUQxMDktMDAwMDAwMiIsImF1ZCI6WyJDSVMtU0VSVkVSIiwiU1RPUkUtRlJPTlQiLCJhY2NvdW50Il0sInN1YiI6IjI3MDI5ZTU5LWM4MTUtNDVkMC1hNzA2LTgwY2I2NzY1MDQwMSIsInR5cCI6IkJlYXJlciIsImF6cCI6Ik1BUktFVC1QTEFDRSIsInNlc3Npb25fc3RhdGUiOiI5NjdkMDE2MS0yZGIxLTQ4OGUtOWRjZS1mNTFkZDNmN2Q1NTkiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vMTkyLjE2OC4xLjg3IiwiaHR0cDovLzE5Mi4xNjguMy4xNzgiLCJodHRwczovL2xvY2FsaG9zdDo4MzgzIiwiaHR0cDovL2xvY2FsaG9zdDo4OTg5IiwiaHR0cDovLzE5Mi4xNjguMS44MyIsImh0dHA6Ly8xOTIuMTY4LjEuODIiLCJodHRwOi8vMTkyLjE2OC4zLjE3MDo4MzgzIiwiKiIsImh0dHA6Ly8xOTIuMTY4LjEuODUiLCJodHRwOi8vbG9jYWxob3N0IiwiaHR0cDovLzE5Mi4xNjguMS44NyIsImh0dHA6Ly8xOTIuMTY4LjEuODgiLCIxOTIuMTY4LjEuODciLCJodHRwOi8vbG9jYWxob3N0OjgzODMiLCJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJodHRwOi8vMTkyLjE2OC4zLjE3MCIsImh0dHBzOi8vMTkyLjE2OC4xLjEzNSJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiTUFSS0VULVBMQUNFIjp7InJvbGVzIjpbIm1hcmtldF9yb2xlIl19LCJDSVMtU0VSVkVSIjp7InJvbGVzIjpbImFwaV91c2VyIl19LCJTVE9SRS1GUk9OVCI6eyJyb2xlcyI6WyJ1c2VyX3JvbGUiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJ1bWFfcHJvdGVjdGlvbiIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiVXNlckF0dHJpYnV0ZU1hcHBlciIsInNpZCI6Ijk2N2QwMTYxLTJkYjEtNDg4ZS05ZGNlLWY1MWRkM2Y3ZDU1OSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhZGRyZXNzIjp7fSwicGFydG5lckNvZGUiOiJjaXMtcC1EMTA5LTAwMDAwMDIiLCJmaXJzdExvZ2luIjoiTiIsInZhbGlkYXRlQ3VycmVudFBhc3N3b3JkIjpmYWxzZSwidG9rZW5Jc3N1ZURhdGUiOjE2NzQ0NjgwMzc0OTIsInByZWZlcnJlZF91c2VybmFtZSI6Im1wIiwibG9jYWxlIjoiZW4iLCJnaXZlbl9uYW1lIjoiTVAiLCJhZ2VudFVzZXIiOiJmYWxzZSIsInRva2VuIjoiZTlmYWVkMjVlNzc4NzczZDNkNGUwNzA5Zjc2Njg0ODIiLCJuYW1lIjoiTVAgTVBfIiwidXNlclR5cGUiOiJwb3J0YWxfdXNlciIsInBhc3NUZW1wIjoiTk9fREFUQSIsInBhc3NJc3N1ZURhdGUiOjAsImZhbWlseV9uYW1lIjoiTVBfIiwiZW1haWwiOiJtcEBtcC5tcCJ9.SpIBsqKeptxgZDCTer-6tZ6lrDlsQFIHXAOKIfT1LtQmSaUSljMU7DtoNe2CnMp2rDCkQtt18ZJEAIdldWshtZejCWIBnXM7VKRsvR2-8BbA0vJQI7RUcD-pfRM9eIa8SGbFpGlNDTNycVlFAyuY_ln2GBF1vyAqcXJtjYpqPeOqHWW3YUE8DZpWZIfaS3ZZLE5a13Dn66g-08EuIZyP-5kmczlpL0MRrhgJJp4AxNo3xhBBkk7KtjtW7fKA2V-Xns1-K2uW-UQAlUTT5-uQ9bHEq1-O7weMoq1Mnxm3OYjGHN_tt52QFjX_pQdDON04nF8CAESTJ1Yox9J5z6vQaA"
+        "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJaOUF1bG9iSlQ2Mmd2al9mX1VwOGp0elcwVWM4dGRiVF8zYkZab3NSbWJvIn0.eyJleHAiOjE2Nzg1MjcwMDMsImlhdCI6MTY3ODM1NDIwMywianRpIjoiZGM3MDBmNjctODc4Yi00ZjRlLTljNjctNTY1ZmQwZDhhNzE3IiwiaXNzIjoiaHR0cDovLzE5Mi4xNjguMS44NDo4MDgwL2F1dGgvcmVhbG1zL2Npcy1wLUQxMDktMDAwMDAwMiIsImF1ZCI6WyJDSVMtU0VSVkVSIiwiU1RPUkUtRlJPTlQiLCJhY2NvdW50Il0sInN1YiI6IjI3MDI5ZTU5LWM4MTUtNDVkMC1hNzA2LTgwY2I2NzY1MDQwMSIsInR5cCI6IkJlYXJlciIsImF6cCI6Ik1BUktFVC1QTEFDRSIsInNlc3Npb25fc3RhdGUiOiI5Y2M5Y2ExYy1iYzNmLTRjNDAtODk4Yi05YTY2MjVkYTE3OWQiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vMTkyLjE2OC4xLjg3IiwiaHR0cDovLzE5Mi4xNjguMy4xNzgiLCJodHRwczovL2xvY2FsaG9zdDo4MzgzIiwiaHR0cDovL2xvY2FsaG9zdDo4OTg5IiwiaHR0cDovLzE5Mi4xNjguMS44MyIsImh0dHA6Ly8xOTIuMTY4LjEuODIiLCJodHRwOi8vMTkyLjE2OC4zLjE3MDo4MzgzIiwiKiIsImh0dHA6Ly8xOTIuMTY4LjEuODUiLCJodHRwOi8vbG9jYWxob3N0IiwiaHR0cDovLzE5Mi4xNjguMS44NyIsImh0dHA6Ly8xOTIuMTY4LjEuODgiLCIxOTIuMTY4LjEuODciLCJodHRwOi8vbG9jYWxob3N0OjgzODMiLCJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJodHRwOi8vMTkyLjE2OC4zLjE3MCIsImh0dHBzOi8vMTkyLjE2OC4xLjEzNSJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiTUFSS0VULVBMQUNFIjp7InJvbGVzIjpbIm1hcmtldF9yb2xlIl19LCJDSVMtU0VSVkVSIjp7InJvbGVzIjpbImFwaV91c2VyIl19LCJTVE9SRS1GUk9OVCI6eyJyb2xlcyI6WyJ1c2VyX3JvbGUiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJ1bWFfcHJvdGVjdGlvbiIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiVXNlckF0dHJpYnV0ZU1hcHBlciIsInNpZCI6IjljYzljYTFjLWJjM2YtNGM0MC04OThiLTlhNjYyNWRhMTc5ZCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhZGRyZXNzIjp7fSwicGFydG5lckNvZGUiOiJjaXMtcC1EMTA5LTAwMDAwMDIiLCJmaXJzdExvZ2luIjoiTiIsInZhbGlkYXRlQ3VycmVudFBhc3N3b3JkIjpmYWxzZSwidG9rZW5Jc3N1ZURhdGUiOjE2NzQ0NjgwMzc0OTIsInByZWZlcnJlZF91c2VybmFtZSI6Im1wIiwibG9jYWxlIjoiZW4iLCJnaXZlbl9uYW1lIjoiTVAiLCJhZ2VudFVzZXIiOiJmYWxzZSIsInRva2VuIjoiZTlmYWVkMjVlNzc4NzczZDNkNGUwNzA5Zjc2Njg0ODIiLCJuYW1lIjoiTVAgTVBfIiwidXNlclR5cGUiOiJwb3J0YWxfdXNlciIsInBhc3NUZW1wIjoiTk9fREFUQSIsInBhc3NJc3N1ZURhdGUiOjAsImZhbWlseV9uYW1lIjoiTVBfIiwiZW1haWwiOiJtcEBtcC5tcCJ9.XOXA3oIhmsQPKZO6wLEk8f6vNKI9UUl1HNEkoCixJeab0k2YiFqzvmPfU5AUqah1azSnLpX7ymZqsXtPX8tZmkPF7mRghZHaVyXyhpfHxW6FW2bu1wfgOnehJGopAcFxTiDt3fMWViF3b8XYxZ881Y6KQO4kpQX2rNKTgiRc2yevg-3lZQDvyGj-SmnLxNAdaic4OgNrzwOAivWboOIszjQteW8RXAr8Ok66lDGjgYCauEj4-8954w_xwzwiFhc1FCEFITQsVddhNQjrXlqn-8mBAYP7Acl8KqjeEBCyLuNQfYIxDE1T5Uhyf_J2IMpC0jGQxH1Y7oDevxez3ExsPA"
     };
     constructor(private http: HttpClient) { }
 
     public getCategories(): Observable<any> {
-        
+
         const body = {
             "length": -1
         };
-        return this.http.post<any>(`${this.host}rest/api/v1/customers/searchCustomerCategories`, body, {headers:this.headers});
+        return this.http.post<any>(`${this.host}rest/api/v1/customers/searchCustomerCategories`, body, { headers: this.headers });
     }
 
-    
+
     public getCycles(): Observable<any> {
         const body = {
             "length": -1
         };
-        return this.http.post<any>(`${this.host}rest/api/v1/customers/searchCycles`, body, {headers:this.headers});
+        return this.http.post<any>(`${this.host}rest/api/v1/customers/searchCycles`, body, { headers: this.headers });
     }
 
     public getPricebook(): Observable<any> {
         const body = {
             "length": -1
         };
-        return this.http.post<any>(`${this.host}rest/api/v1/customers/searchPricePlans`, body, {headers:this.headers});
+        return this.http.post<any>(`${this.host}rest/api/v1/customers/searchPricePlans`, body, { headers: this.headers });
     }
 
-    public search(body : any): Observable<any> {
-        return this.http.post<any>(`${this.host}rest/api/v1/ui/searchCustomersBasic`, body, {headers:this.headers});
+    public search(body: any): Observable<any> {
+        return this.http.post<any>(`${this.host}rest/api/v1/ui/searchCustomersBasic`, body, { headers: this.headers });
     }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,9 +2,12 @@ import {Component} from '@angular/core';
 
 @Component({
     template: `
-    <h1 style='font-size:7rem;text-align:center;width:100vw'>
-      i am a Developer (✿◡‿◡)
-    </h1>
+    <h3 style='font-size:7rem;text-align:center;width:100vw'>
+      i am not a QA <h3 class="angry" >( ˘︹˘ )</h3>
+    </h3>
+    <h3 style='font-size:7rem;text-align:center;width:100vw'>
+    i am a Developer <h3 class="happy" >(✿◡‿◡)</h3>
+  </h3>
   `
 })
 export class HomeComponent {
